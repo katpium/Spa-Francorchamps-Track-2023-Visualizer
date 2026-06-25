5666f0337bf78858f8d8af19810980865d0cdf90
katpium <uwuing88@gmail.com>
2026-06-25 09:58:55 +0700

    Spa-Fran Race Website
    Create a website that shows the Spa-Fran 2023 race

---
+++ tutorials/SpaRaceWebsite/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/api/location", async () =>
+{
+    HttpClient client = new HttpClient();
+    int spaRaceSessionKey = 9141;
+    int driverNumber = 1;
+
+    string locationUrl = $"https://api.openf1.org/v1/location?session_key={spaRaceSessionKey}&driver_number={driverNumber}";
+
+    List<LocationPoint>? locations = await client.GetFromJsonAsync<List<LocationPoint>>(locationUrl);
+
+    if (locations == null || locations.Count == 0)
+    {
+        return Results.NotFound("No location data found.");
+    }
+
+    return Results.Ok(locations.Take(100));
+});
+
+Console.WriteLine ("=== Spa-Francorchamps 2023 Race ===");
+Console.WriteLine();
+
+app.Run();
+
+public class LocationPoint
+{
+    [JsonPropertyName("date")]
+    public string Date { get; set; } = "";
+
+    [JsonPropertyName("driver_number")]
+    public int DriverNumber { get; set; }
+
+    [JsonPropertyName("session_key")]
+    public int SessionKey { get; set; }
+
+    [JsonPropertyName("x")]
+    public double X { get; set; }
+
+    [JsonPropertyName("y")]
+    public double Y { get; set; }
+
+    [JsonPropertyName("z")]
+    public double Z { get; set; }
+}
